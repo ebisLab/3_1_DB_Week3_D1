b51ee18b107dc4fd51903a10e2b1742915a12d82
ebisLab <ebaiden@techie.com>
2019-09-16 19:19:38 -0400

    post request properly called

--- register/register-router.js
+++ register/register-router.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const Register = require('./register-model.js');
-// const router = express.Router();
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send("It's alive!");
 });
-router.get('/register', (req, res) => {
+
+router.get('/', (req, res) => {
 
     const name = req.query.name
-    // const hash = ''
     const hash = bcrypt.hashSync(name, 14) //the highest the number the most secure
     res.send(`The hash for ${name} is ${hash}`)
 
